3e6f00565310c7da76ef839c1e0081b7a330fd30
Kenshi-Sama <erikdasilvaferreira@hotmail.com>
2023-05-22 15:59:56 -0300

    adicionando arquivo js

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+/*
+O que precisamos fazer? - Quando clicarmos no botão de troca de tema, temos que alterar a cor do tema da página para as cores do tema claro ou do tema escuro.
+
+Objetivo1 - Quando clicar no botão de troca de tema, adicionar a classe .modo-escuro no body, para que os estilos do modo escuro sejam aplicados e mude a imagem para lua.
+
+    -Passo1 - Pegar no JS o elemento HTML correspondente ao botão de troca de tema
+    -Passo2 - Pegar no JS o elemento HTML correspondente ao body
+    -Passo3 - Identificar o clique do usuário no botão de troca de tema 
+    -Passo4 - Adicionar a classe .modo-escuro no body
+    -Passo5 - trocar a imagem do botão de alterar o tema para lua
+
+Objetivo2 - Quando clicar no botão de troca de tema, caso o body já tenha a classe modo-escuro, remover a classe para mudar para o modo claro e mudar a imagem para o sol.
+*/
